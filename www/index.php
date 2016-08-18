7b7484a85333c5d4f150488717f027fd7b6f351f
Evgenii Sopov <mrseakg@gmail.com>
2016-08-18 19:34:44 +0700

    added www server for synchronization

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Reverse Hash</title>
+</head>
+<body>
+	<table width="100%" height="100%">
+		<tr>
+			<td>
+				<center>
+					<h1>Reverse MD5</h1>
+					<input type="text" size="55"> <button id="try" onclick="alert('TODO');">Try</button>
+				</center>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
