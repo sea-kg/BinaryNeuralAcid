b19491d51c1674fa319a9d6651911cbe3e214474
Evgenii Sopov <mrseakg@gmail.com>
2018-01-07 17:24:27 +0700

    Updated www

--- var/www/html/reversehashd/js/index.js
+++ var/www/html/reversehashd/js/index.js
@@ -1,36 +1,112 @@
+
+window.reversehashd.handlerTrainingThreadInfo = function(response){
+	console.log(response);
+	var status = "Bit (" + response.bitid + "): " + response.status;
+	if(response.percent){
+		status += " (" + response.percent + "%)";
+	}
+	$('#training_message').html(status);
+	
+	$('#' + response.bitid + " .info .status").html(status);
+	
+	if(response.lp){
+		updatePercents(response.bitid, response.lp);
+	}
+
+	$('#' + response.bitid + " .info .bitprogress").show();
+	if(response.completed){
+		$('#' + response.bitid + " .info .bitprogress-ins").css({'width': response.completed + '%'});
+	}
+}
+
+reversehashd.initWebsocket();
+
+function try_reverse(){
+	$('#reverse_result').html('');
+	showLoading();
+	reversehashd.reverse($('#input_md5').val()).done(function(r){
+		console.log("done");
+		console.log("response: ", r);
+		setTimeout(function(){
+			hideLoading();
+			$('#reverse_result').html('');
+			$('#reverse_result').append(compareHashes(r.request_md5, r.result_md5));
+			if(r.request_md5 == r.result_md5){
+				$('#reverse_result').append('Cool!!! I found it:\n');
+				$('#reverse_result').append('Base64: ' + r.answer_base64 + "\n");
+				$('#reverse_result').append('HEX: ' + r.answer_hex + "\n");
+				$('#reverse_result').append('Text: ' + r.answer_text + "\n");
+			}else{
+				$('#reverse_result').append('\nNot found matches.\n\n Response hex is: \n' + prepareHex(r.answer_hex) + "\n");
+			}
+		},2000);
+		hideLoading();
+	}).fail(function(r){
+		console.log("fail");
+		setTimeout(function(){
+			hideLoading();
+			$('#reverse_result').html("Error " + r.code + "): " + r.error);
+		},2000);
+	});
+}
+
+function updatePercents(bitid, lp){
+	var cl = 'alert-info';
+	if(lp == 100){
+		cl = 'alert-success';
+	}else if(lp < 20){
+		cl = 'alert-danger';
+	}
+
+	$('#bit' + bitid).removeClass('alert-success');
+	$('#bit' + bitid).removeClass('alert-info');
+	$('#bit' + bitid).removeClass('alert-danger');
+	$('#bit' + bitid).addClass(cl);
+	$('#bit' + bitid + " .lp").html(lp + "%");
+}
+
+function loadStatistics(){
+	reversehashd.statistics().done(function(r){
+		// console.log(r);
+		$('#statistics_page').html('');
+		
+		for(var i = 0; i < r.statistics.length; i++){
+			var sbit = r.statistics[i];
+			$('#statistics_page').append(''
+				+ '<div class="card card-bit" id="' + sbit.name + '">'
+				+ '	<div class="card-body">'
+				+ '	<h5 class="card-title">' + sbit.name + '</h4>'
+				+ '	<p class="card-text">' + sbit.modified + '</p>'
+				+ '	<p class="card-text lp">' + sbit.lp + '%</p>'
+				// + ' <a href="#" class="btn btn-primary">Go somewhere</a>'
+				+ '</div>'
+				+ '</div>'
+			);
+
+			updatePercents(sbit.name, sbit.lp);
+		}
+	}).fail(function(r){
+		console.error(r);
+		$('#statistics_page').html('failed');
+	});
+}
+
+
 $(document).ready(function(){
-	reversehashd.initWebsocket();
+	$('#try_reverse').unbind().bind('click', try_reverse);
+	
 	$('#input_md5').keypress(function (e) {
 		if (e.which == 13) {
-			$('#result').html('');
-			showLoading();
-			reversehashd.reverse($('#input_md5').val()).done(function(r){
-				console.log("done");
-				console.log("response: ", r);
-				setTimeout(function(){
-					hideLoading();
-					$('#result').html('');
-					$('#result').append(compareHashes(r.request_md5, r.result_md5));
-					if(r.request_md5 == r.result_md5){
-						$('#result').append('Cool!!! I found it:\n');
-						$('#result').append('Base64: ' + r.answer_base64 + "\n");
-						$('#result').append('HEX: ' + r.answer_hex + "\n");
-						$('#result').append('Text: ' + r.answer_text + "\n");
-					}else{
-						$('#result').append('\nNot found matches.\n\n Response hex is: \n' + prepareHex(r.answer_hex) + "\n");
-					}
-				},2000);
-				hideLoading();
-			}).fail(function(r){
-				console.log("fail");
-				setTimeout(function(){
-					hideLoading();
-					$('#result').html("Error " + r.code + "): " + r.error);
-				},2000);
-			});
+			try_reverse();
 			return false;
 		}
 	});
+	
+	$('#show_statistics').unbind().bind('click', function(){
+		$('#reverse_page').hide();
+		$('#statistics_page').show();
+		loadStatistics();
+	})
 });
 
 
