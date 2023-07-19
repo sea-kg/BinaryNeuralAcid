158b000107e00ec4ec96465b3e52099954cb3b50
Evgenii Sopov <mrseakg@gmail.com>
2023-07-19 11:56:43 +0700

    Redesign unit-test from wsjcpp to cmake test

---
+++ tests/test_read_write.cpp
@@ -0,0 +1,53 @@
+#include "binary_neural_acid.h"
+
+#include <vector>
+#include <wsjcpp_core.h>
+
+int main() {
+    WsjcppCore::makeDir("./temporary-unit-tests-data");
+
+    BNA bna;
+    bna.randomGenerate(128, 1, 256);
+
+    // fill the input vector
+    std::vector<BinaryNeuralAcidBit> vInputs;
+    for (int i = 0; i < 128; i++) {
+        vInputs.push_back(B_1);
+    }
+    BinaryNeuralAcidBit bResult = bna.calc(vInputs, 0);
+
+    std::string sPath0 = "./temporary-unit-tests-data/read-write-test0";
+    if (!bna.save(sPath0)) {
+        std::cerr << "Could not save to file: " << sPath0 << std::endl;
+        return 1;
+    }
+
+    if (!bna.load(sPath0)) {
+        std::cerr << "Could not load from file: " << sPath0 << std::endl;
+        return 1;
+    }
+
+    BinaryNeuralAcidBit bResultNew0 = bna.calc(vInputs, 0);
+    if (bResult != bResultNew0) {
+        std::cerr << "Different result for " << sPath0 << std::endl;
+        return 1;
+    }
+
+    std::string sPath1 = "./temporary-unit-tests-data/read-write-test1";
+    if (!bna.save(sPath1)) {
+        std::cerr << "Could not save to file: " << sPath1 << std::endl;
+        return 1;
+    }
+
+    if (!bna.load(sPath1)) {
+        std::cerr << "Could not load from file: " << sPath1 << std::endl;
+        return 1;
+    }
+
+    BinaryNeuralAcidBit bResultNew1 = bna.calc(vInputs, 0);
+    if (bResult != bResultNew1) {
+        std::cerr << "Different result for " << sPath1 << std::endl;
+        return 1;
+    }
+    return 0;
+}
