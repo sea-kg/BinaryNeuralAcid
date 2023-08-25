b492f58f361230a2e0d06c42840228aa1322a2ca
Evgenii Sopov <mrseakg@gmail.com>
2023-08-25 23:20:48 +0700

    Moved learning test to ctest

---
+++ tests/test_learning.cpp
@@ -0,0 +1,64 @@
+#include "binary_neural_acid.h"
+
+#include <vector>
+
+int check_xor_result(BinaryNeuralAcid<BinaryNeuralAcidBit> &bna) {
+
+    int nRet = 0;
+    if (bna.compute({BinaryNeuralAcidBit::B_0, BinaryNeuralAcidBit::B_0}, 0) != BinaryNeuralAcidBit::B_0) {
+        nRet++;
+    }
+
+    if (bna.compute({BinaryNeuralAcidBit::B_0, BinaryNeuralAcidBit::B_1}, 0) != BinaryNeuralAcidBit::B_1) {
+        nRet++;
+    }
+
+    if (bna.compute({BinaryNeuralAcidBit::B_1, BinaryNeuralAcidBit::B_0}, 0) != BinaryNeuralAcidBit::B_1) {
+        nRet++;
+    }
+
+    if (bna.compute({BinaryNeuralAcidBit::B_1, BinaryNeuralAcidBit::B_1}, 0) != BinaryNeuralAcidBit::B_0) {
+        nRet++;
+    }
+    return nRet;
+};
+
+int main() {
+
+    std::vector<std::pair<std::string, std::string>> vTestData;
+    BinaryNeuralAcidConfig config;
+    config
+        .setInputSize(2)
+        .setNodesSize(5)
+        .setOutputSize(1)
+    ;
+
+    BinaryNeuralAcid<BinaryNeuralAcidBit> bna;
+    bna.setPseudoRandom(new BinaryNeuralAcidPseudoRandomSin());
+    bna.randomGenerate(config);
+
+    int nMutationCicles = 10;
+    int nAddCicles = 1024;
+    int nRemoveCicles = 20;
+    auto pModelModify = new BinaryNeuralAcidModificationModel(nMutationCicles, nAddCicles, nRemoveCicles);
+
+    int res = check_xor_result(bna);
+    int nSafeCicles = 0;
+    while (res != 0) {
+        nSafeCicles++;
+        if (nSafeCicles > 100) {
+            std::cout << "res " << res << std::endl;
+            return 1;
+        }
+        bna.randomModify(pModelModify);
+        bna.removeAllDeadlockNodes();
+        res = check_xor_result(bna);
+        if (res == 0) {
+            std::cout << " Found on cicle: " << nSafeCicles << std::endl;
+            bna.exportToDot("xor");
+            return 0;
+        }
+    }
+
+    return 1;
+}
